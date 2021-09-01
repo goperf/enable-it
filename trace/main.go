ceacc67f6fbb4326513eb925d624d34a5c22f91a
srimal_n <srimal@pickme.lk>
2021-09-01 13:02:12 +0530

    add  example for trace

---
+++ trace/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	_ "net/http/pprof"
+)
+
+/*
+Notes: You can open the webpage on http://localhost:6060/debug/pprof
+*/
+func main() {
+
+	// Start profiling server in a separate goroutine so it does not block
+	go func() {
+		http.ListenAndServe(":6060", nil)
+	}()
+
+	for {
+		Fib(10)
+	}
+
+}
+
+func Fib(num int) int {
+	if num < 2 {
+		return 0
+	}
+
+	return Fib(num-2) + Fib(num-1)
+}
